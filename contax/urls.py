142c9d05d36162dedd559cfc5193415391277d5c
Matthew Lemon <y@yulqen.org>
2025-02-17 21:24:59 +0000

    Changes based URL for the contacts app

--- contax/urls.py
+++ contax/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("contacts/", include("contacts.urls")),
+    path("", include("contacts.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
